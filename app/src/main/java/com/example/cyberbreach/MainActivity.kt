7ace3c36cafc5a2402aa60ae7fc180aee61ad4bb
Siffran <davidaxelbenjaminsson@gmail.com>
2021-01-31 22:05:17 +0100

    capture picture is displayed in the imageView but rotation is incorrect

--- app/src/main/java/com/example/cyberbreach/MainActivity.kt
+++ app/src/main/java/com/example/cyberbreach/MainActivity.kt
@@ -2,36 +2,29 @@ package com.example.cyberbreach
 
 import android.Manifest
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.view.isInvisible
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textview.MaterialTextView
-import com.google.mlkit.vision.common.InputImage
+import com.google.common.util.concurrent.ListenableFuture
 import com.google.mlkit.vision.text.Text
-import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.TextRecognizer
 import kotlinx.android.synthetic.main.main_activity.*
-import java.io.File
 import java.nio.ByteBuffer
-import java.util.ArrayDeque
-import java.util.ArrayList
 import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
-typealias LumaListener = (luma: Double) -> Unit
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,20 +37,15 @@ class MainActivity : AppCompatActivity() {
         private const val RATIO_16_9_VALUE = 16.0 / 9.0
 
     }
-    private var imageCapture: ImageCapture? = null
-
-    private lateinit var outputDirectory: File
-    private lateinit var cameraExecutor: ExecutorService
-
     private lateinit var templateTextView: MaterialTextView
     private lateinit var mImageView: ImageView
-    private lateinit var btnStartBreach: MaterialButton
 
-    private var camera: Camera? = null
+    //////////////////////////////////////////////////////////
     private var preview: Preview? = null
-    private var cameraProvider: ProcessCameraProvider? = null
-    private var imageAnalyzer: ImageAnalysis? = null
+    private var imageCapture: ImageCapture? = null
+    private var camera: Camera? = null
 
+    private lateinit var btnStartBreach : MaterialButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,10 +59,6 @@ class MainActivity : AppCompatActivity() {
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
-
-        //outputDirectory = getOutputDirectory()
-
-        cameraExecutor = Executors.newSingleThreadExecutor()
 
         // elements in fragment
         btnStartBreach = findViewById(R.id.btnStartBreach)
@@ -95,100 +79,57 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera(){
-        Log.i(TAG,"startCamera")
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        Log.i(TAG, "startCamera")
 
+        val cameraProviderFuture : ListenableFuture<ProcessCameraProvider> = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener(Runnable {
-            cameraProvider = cameraProviderFuture.get()
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            bindCameraUseCases()
+            preview = Preview.Builder()
+                    .build()
+
+            imageCapture = ImageCapture.Builder()
+                    .build()
+
+            val cameraSelector: CameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                    .build()
+
+            try {
+                cameraProvider.unbindAll()
+
+                camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+
+                preview?.setSurfaceProvider(cameraView.surfaceProvider)
+
+            } catch (e: Exception) {
+                Log.i(TAG, "startCamera failed")
+            }
+
         }, ContextCompat.getMainExecutor(this))
 
-    }
 
-    private fun bindCameraUseCases() {
-
-        // Get screen metrics used to setup camera for full screen resolution
-        val metrics = DisplayMetrics().also { cameraView.display.getRealMetrics(it) }
-        Log.d(TAG, "Screen metrics: ${metrics.widthPixels} x ${metrics.heightPixels}")
-
-        val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
-        Log.d(TAG, "Preview aspect ratio: $screenAspectRatio")
-
-        val rotation = cameraView.display.rotation
-
-        // CameraProvider
-        val cameraProvider = cameraProvider
-                ?: throw IllegalStateException("Camera initialization failed.")
-
-        // CameraSelector
-        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-        // Preview
-        preview = Preview.Builder()
-                // We request aspect ratio but no resolution
-                .setTargetAspectRatio(screenAspectRatio)
-                // Set initial target rotation
-                .setTargetRotation(rotation)
-                .build()
-
-        // ImageCapture
-        imageCapture = ImageCapture.Builder()
-                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                // We request aspect ratio but no resolution to match preview config, but letting
-                // CameraX optimize for whatever specific resolution best fits our use cases
-                .setTargetAspectRatio(screenAspectRatio)
-                // Set initial target rotation, we will have to call this again if rotation changes
-                // during the lifecycle of this use case
-                .setTargetRotation(rotation)
-                .build()
-
-/*        val mImageAnalyzer = ImageAnalysis.Builder()
-                .setTargetAspectRatio(screenAspectRatio)
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .build()
-
-        mImageAnalyzer.setAnalyzer(cameraExecutor, ImageAnalysis.Analyzer {})*/
-
-        // ImageAnalysis
-        imageAnalyzer = ImageAnalysis.Builder()
-                // We request aspect ratio but no resolution
-                .setTargetAspectRatio(screenAspectRatio)
-                // Set initial target rotation, we will have to call this again if rotation changes
-                // during the lifecycle of this use case
-                .setTargetRotation(rotation)
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .build()
-                // The analyzer can then be assigned to the instance
-                .also {
-                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-                        // Values returned from our analyzer are passed to the attached listener
-
-                        // We log image analysis results here - you should do something useful
-                        // instead!
-
-                        Log.d(TAG, "Average luminosity: $luma")
-                    })
-                }
-
-        // Must unbind the use-cases before rebinding them
-        cameraProvider.unbindAll()
-
-        try {
-            // A variable number of use-cases can be passed here -
-            // camera provides access to CameraControl & CameraInfo
-            camera = cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture, imageAnalyzer)
-
-            // Attach the viewfinder's surface provider to preview use case
-            preview?.setSurfaceProvider(cameraView.surfaceProvider)
-        } catch (exc: Exception) {
-            Log.e(TAG, "Use case binding failed", exc)
-        }
     }
 
     private fun startBreach(){
 
+        val imageCapture : ImageCapture = imageCapture?: return
+
+        imageCapture.takePicture(ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageCapturedCallback() {
+            override fun onCaptureSuccess(image: ImageProxy) {
+                super.onCaptureSuccess(image)
+                Log.i(TAG, "capture success!")
+
+                mImageView.setImageBitmap(imageProxyToBitmap(image))
+
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                super.onError(exception)
+                Log.i(TAG, "capture failed!")
+
+            }
+        })
 
         //capture preview
 
@@ -209,13 +150,6 @@ class MainActivity : AppCompatActivity() {
                 baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
-/*    private fun getOutputDirectory(): File {
-        val mediaDir = externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(androidx.camera.core.R.string.app_name)).apply { mkdirs() } }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
-    }*/
-
     private fun aspectRatio(width: Int, height: Int): Int {
         val previewRatio = max(width, height).toDouble() / min(width, height)
         if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
@@ -224,139 +158,21 @@ class MainActivity : AppCompatActivity() {
         return AspectRatio.RATIO_16_9
     }
 
-    private class LuminosityAnalyzer(listener: LumaListener? = null) : ImageAnalysis.Analyzer {
-
-        override fun analyze(image: ImageProxy) {
-
-            val recognizer = TextRecognition.getClient()
-
-            val mediaImage = image.image
-
-            if (mediaImage != null) {
-                val mImage = InputImage.fromMediaImage(mediaImage, image.imageInfo.rotationDegrees)
-
-                val result = recognizer.process(mImage)
-                        .addOnSuccessListener { texts ->
-                            texts.textBlocks[0].text
-                            Log.i(TAG,"SUCCESS!")
-                        }
-                        .addOnFailureListener { e ->
-                            Log.i(TAG,"FAIL")
-                        }
-                Log.i(TAG, result.toString())
-
-                // Pass image to an ML Kit Vision API
-                // ...
-            }
-
-            Log.i(TAG, "analyzed")
-
-            image.close()
-        }
-    }
-
-
-    /*private class LuminosityAnalyzer(listener: LumaListener? = null) : ImageAnalysis.Analyzer {
-        private val frameRateWindow = 8
-        private val frameTimestamps = ArrayDeque<Long>(5)
-        private val listeners = ArrayList<LumaListener>().apply { listener?.let { add(it) } }
-        private var lastAnalyzedTimestamp = 0L
-        var framesPerSecond: Double = -1.0
-            private set
-
-        *//**
-         * Used to add listeners that will be called with each luma computed
-         *//*
-        fun onFrameAnalyzed(listener: LumaListener) = listeners.add(listener)
-
-        *//**
-         * Helper extension function used to extract a byte array from an image plane buffer
-         *//*
-        private fun ByteBuffer.toByteArray(): ByteArray {
-            rewind()    // Rewind the buffer to zero
-            val data = ByteArray(remaining())
-            get(data)   // Copy the buffer into a byte array
-            return data // Return the byte array
-        }
-
-        *//**
-         * Analyzes an image to produce a result.
-         *
-         * <p>The caller is responsible for ensuring this analysis method can be executed quickly
-         * enough to prevent stalls in the image acquisition pipeline. Otherwise, newly available
-         * images will not be acquired and analyzed.
-         *
-         * <p>The image passed to this method becomes invalid after this method returns. The caller
-         * should not store external references to this image, as these references will become
-         * invalid.
-         *
-         * @param image image being analyzed VERY IMPORTANT: Analyzer method implementation must
-         * call image.close() on received images when finished using them. Otherwise, new images
-         * may not be received or the camera may stall, depending on back pressure setting.
-         *
-         *//*
-        override fun analyze(image: ImageProxy) {
-            // If there are no listeners attached, we don't need to perform analysis
-            if (listeners.isEmpty()) {
-                image.close()
-                return
-            }
-
-            // Keep track of frames analyzed
-            val currentTime = System.currentTimeMillis()
-            frameTimestamps.push(currentTime)
-
-            // Compute the FPS using a moving average
-            while (frameTimestamps.size >= frameRateWindow) frameTimestamps.removeLast()
-            val timestampFirst = frameTimestamps.peekFirst() ?: currentTime
-            val timestampLast = frameTimestamps.peekLast() ?: currentTime
-            framesPerSecond = 1.0 / ((timestampFirst - timestampLast) /
-                    frameTimestamps.size.coerceAtLeast(1).toDouble()) * 1000.0
-
-            // Analysis could take an arbitrarily long amount of time
-            // Since we are running in a different thread, it won't stall other use cases
-
-            lastAnalyzedTimestamp = frameTimestamps.first
-
-            // Since format in ImageAnalysis is YUV, image.planes[0] contains the luminance plane
-            val buffer = image.planes[0].buffer
-
-            // Extract image data from callback object
-            val data = buffer.toByteArray()
-
-            // Convert the data into an array of pixel values ranging 0-255
-            val pixels = data.map { it.toInt() and 0xFF }
-
-            // Compute average luminance for the image
-            val luma = pixels.average()
-
-            // Call all listeners with new value
-            listeners.forEach { it(luma) }
-
-            image.close()
-        }
-    }*/
-
-/*    private class mImageAnalyzer : ImageAnalysis.Analyzer {
-        override fun analyze(imageProxy: ImageProxy) {
-            val mediaImage = imageProxy.image
-
-            if (mediaImage != null) {
-                val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
-                // Pass image to an ML Kit Vision API
-                // ...
-            }
-        }
-    }*/
-
     private fun processTextRecognitionResult(texts: Text?) {
 
-        Log.i(TAG,"Processing imagge!")
+        Log.i(TAG, "Processing imagge!")
 
+    }
+
+    private fun imageProxyToBitmap(image: ImageProxy): Bitmap? {
+        val planeProxy = image.planes[0]
+        val buffer: ByteBuffer = planeProxy.buffer
+        val bytes = ByteArray(buffer.remaining())
+        buffer.get(bytes)
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        cameraExecutor.shutdown()
     }
 }
